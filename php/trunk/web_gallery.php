1422555c577b27724c62049101405fb992cd34c1
Thorsten Kohlhepp <thorko@thorko.de>
2012-07-14 19:26:46 +0200

    missed a path

--- php/trunk/web_gallery.php
+++ php/trunk/web_gallery.php
@@ -45,7 +45,7 @@ hs.addSlideshow({
 $thumb = new Picture();
 
 for($i = 0; $i < sizeof($pics); $i++) {
-	if ( ! file_exists("$path/$pics[$i]") ){
+	if ( ! file_exists("$path_thumb/$pics[$i]") ){
 		$thumb->createThumb("$path_pics/$pics[$i]", "$path_thumb/$pics[$i]", 150);
 	}
 	echo "<a href=\"$path_pics/$thumbs[$i]\" class=\"highslide\" onclick=\"return hs.expand(this, { captionEval: 'this.thumb.alt' })\"><img src=\"$path_thumb/$pics[$i]\" alt=\"".$thumb->getComment($path_pics/$pics[$i])."\" title=\"Click to enlarge\" /></a>\n";
