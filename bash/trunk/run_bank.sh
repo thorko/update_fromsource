19cd73a7ad5a112d3f87fc8c5ff60784c00b1346
Thorsten Kohlhepp <thorko@thorko.de>
2013-04-20 12:23:17 +0200

    check dkb online banking as well

--- bash/trunk/run_bank.sh
+++ bash/trunk/run_bank.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 cipher="AES256"
-file=/opt/scripts/perl/trunk/get_bank_status.pl.pgp
+file[0]=/opt/scripts/perl/trunk/get_bank_status_comdirect.pl.pgp
+file[1]=/opt/scripts/perl/trunk/get_bank_status_dkb.pl.pgp
 documents=/home/thorko/Documents/bank
 
 function decrypt_and_run () {
-	fn="${file%.*}"
-	echo $fn
-	gpg -d --no-use-agent -o $fn $file
-	perl $fn
-	rm $fn
+	for l in "${file[@]}"; do
+		fn="${l%.*}"
+		echo $fn
+		gpg -d --no-use-agent -o $fn $l
+		perl $fn
+		rm $fn
+	done
 }
 
 function show () {
-	# get latest document
-	fn="${file%.*}"
-        gpg -d --no-use-agent -o $fn $file
-        perl $fn -s
-	rm -f $fn
+	for l in "${file[@]}"; do
+		fn="${l%.*}"
+        	gpg -d --no-use-agent -o $fn $l
+        	perl $fn -s
+		rm -f $fn
+	done
 }
 
 function clean () {
 	cd $documents
 	find . -name "*.html" -exec rm -f {} \;
+	find . -name "*.csv" -exec rm -f {} \;
 }
 
 option=$1
