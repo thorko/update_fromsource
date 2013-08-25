b6ffc7e9a1c8b00f2882ae650162aad107785d73
Thorsten Kohlhepp <info@thorko.de>
2013-08-25 21:52:50 +0200

    configure apache2

--- bash/trunk/create-new-site.sh
+++ bash/trunk/create-new-site.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+source colors
 
 # Your sitename
 echo -n "What domain should the site created for: "
 read site
 
 # create new site
-
-
 # apache config
+cp /etc/apache2/sites-available/skeleton /etc/apache2/sites-available/$site
+cp /etc/apache2/sites-available/ssl-skeleton /etc/apache2/sites-available/ssl-$site
+
+a2ensite $site
+a2ensite ssl-$site
+
+apachectl -t 
+if [ $? -eq 0 ]; then 
+  apachectl graceful
+else
+  echo -e "${red}Couldn't restart apache. Config issue${nc}"
+fi
 
 
 # postfix config
@@ -18,7 +29,7 @@ perl -i -e "s/^(mydestination =.*)/\$1, $site/g" /etc/postfix/main.cf
  if [ $? -eq 0 ]; then
   /etc/init.d/postfix reload
  else 
-  echo "Couldn't reload postfix configuration. Postmap has failed."
+  echo -e "${red}Couldn't reload postfix configuration. Postmap has failed.${nc}"
  fi
 
 # 
